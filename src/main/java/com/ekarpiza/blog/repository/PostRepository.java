4047d40335d3675b3b6e12d401260da39e901ca8
EgorKarpiza <ekarpiza@yandex.ru>
2023-03-03 19:10:08 +0700

    end

---
+++ src/main/java/com/ekarpiza/blog/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.ekarpiza.blog.repository;
+
+import com.ekarpiza.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
